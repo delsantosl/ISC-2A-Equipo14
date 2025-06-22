8c3094f1ad3fd5b6e1741bc1ff77c806f3ef7b12
delsantosl <al552909@edu.uaa.mx>
2025-06-22 17:20:37 -0600

    Matriz dinamica
    Creacion de matriz de manera dinamica y empezar con la matriz llena de espacios

--- Ejercicio6_Scrabble.cpp
+++ Ejercicio6_Scrabble.cpp
@@ -6,26 +6,37 @@ using namespace std;
 
 enum orientacion{ VERTICAL, HORIZONTAL};
 
-bool colocarPalabra(char T[TAM][TAM], int f, int c, orientacion o, char *palabra);
-bool estaVacio(char T[TAM][TAM]);
+bool colocarPalabra(char **, int f, int c, orientacion o, char *palabra);
+bool estaVacio(char **;
 bool esPalabra(char *palabra);
 
 int main(){
-	char T[TAM][TAM];
+	char T**;
+	//Crear matriz dinamica
+	T=(char**)calloc(TAM,sizeof(char*);
+	for(int i=0; i<TAM; i++){
+		*(t+1)=(char*)calloc(TAM,sizeof(char);
+	}
+	// Empezar con el tablero con espacios
+	for(int i=0; i<TAM; i++){
+		for(int j=0; j<TAM; j++){
+			*(*(t+i)+j)==' ';
+		}
+	}
 	
 	
 	return 0;
 }
 
-bool estaVacio(char T[TAM][TAM]){
+bool estaVacio(char T**){
 	for(int i=0; i<TAM; i++){
 		for(int j=0; j<TAM; j++){
 			if(T[i][j] != ' '){
 				return false;
 			}
-				else{
-					return true;
-				}
+			else{
+				return true;
+			}
 		}
 	}
 }
